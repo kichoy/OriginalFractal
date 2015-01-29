33f83f277902da7454c03f2f6b296778c51b0d52
kichoy <kichoy@697-334-Erebor.sf.k12.internal>
2015-01-29 10:36:21 -0800

    testing w ben441318936 fractal

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
 public double rad=(float)(Math.PI/4);
 public double screenL=500;
 public double sideLength1=1; //screenL*Math.sqrt(2)/2+100
@@ -22,7 +38,7 @@ public void draw()
 	b=255;
 	inscribedSquares(0,0,sideLength1,numSquares);
 	sideLength1++;
-	sideLength1*=1.01;
+	sideLength1*=1.01f;
 	if(sideLength1>=screenL/Math.pow(Math.sqrt(2)/2, numSquares-1)+100)
 	{
 		sideLength1=1;
@@ -77,3 +93,12 @@ public void inscribedSquares(double x, double y, double sideL, int nSquares)
 			inscribedSquares(x,y,sideL,nSquares);
 		}
 	}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
