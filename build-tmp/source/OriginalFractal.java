b7dc2b57681683faa978eef6268a8d84404cd08c
kichoy <kichoy@697-334-Erebor.sf.k12.internal>
2015-02-05 09:53:31 -0800

    starting it out

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -14,85 +14,43 @@ import java.io.IOException;
 
 public class OriginalFractal extends PApplet {
 
-public double rad=(float)(Math.PI/4);
-public double screenL=500;
-public double sideLength1=1; //screenL*Math.sqrt(2)/2+100
-public int numSquares=12;
-public int r=220;
-public int g=122;
-public int b=255;
+//drawing parameters
+int detail = 30; //makes triangle more 'detailed' by recursive func
+int sizeSier = detail*10;
+
+
 public void setup()
 {
-	size((int)screenL,(int)screenL);
-	background(0);
-	rectMode(RADIUS); //all squares are drawn with specified center and radius
-	smooth();
+	background(100, 5);
+	size(sizeSier+sizeSier/4, sizeSier+sizeSier/4); //fits the triangle
 }
 
+public int count = 5;
 public void draw()
 {
-	translate((float)(screenL/2), (float)(screenL/2));
-	rad=(float)(Math.PI/4);
-	r=220;
-	g=122;
-	b=255;
-	inscribedSquares(0,0,sideLength1,numSquares);
-	sideLength1++;
-	sideLength1*=1.01f;
-	if(sideLength1>=screenL/Math.pow(Math.sqrt(2)/2, numSquares-1)+100)
-	{
-		sideLength1=1;
-		numSquares++;
-	}
+	sierpinski(sizeSier/8, sizeSier+sizeSier/8, sizeSier);
 }
 
-public void inscribedSquares(double x, double y, double sideL, int nSquares)
+public void mouseMoved()//optional
 {
-	if(nSquares>0)
+	//sizeSier++;
+	detail = mouseX/2;
+}
+
+public void sierpinski(int x, int y, int len) //recursive func to draw sierpinski triangle
+{
+	if (len > detail)
 	{
-		rotate((float)rad);
-		if(nSquares==numSquares) { }
-			else if(nSquares>numSquares-1-1*(numSquares-2)/5)
-			{
-				r=r-98/((numSquares-2)/5);
-			}
-			else if(nSquares>numSquares-1-2*(numSquares-2)/5)
-			{
-				r=122;
-				g=g+133/((numSquares-2)/5);
-			}
-			else if(nSquares>numSquares-1-3*(numSquares-2)/5)
-			{
-				g=255;
-				b=b-133/((numSquares-2)/5);
-			}
-			else if(nSquares>numSquares-1-4*(numSquares-2)/5)
-			{
-				b=122;
-				r=r+133/((numSquares-2)/5);
-			}
-			else if(nSquares>numSquares-1-5*(numSquares-2)/5)
-			{
-				r=255;
-				g=g-133/((numSquares-2)/5);
-			}
-			else
-			{
-				r=0; g=0; b=0;
-			}
-			fill(r,g,b);
-			stroke(0);
-			rect((float)x,(float)y,(float)sideL/2,(float)sideL/2);
-			rotate((float)rad);
-			nSquares--;
-		}
-		if(nSquares>0)
-		{
-			rad=rad+(double)(Math.PI/4);
-			sideL=sideL*(Math.sqrt(2)/2);
-			inscribedSquares(x,y,sideL,nSquares);
-		}
+		sierpinski(x, y, len/2);
+		sierpinski(x+len/2, y, len/2);
+		sierpinski(x+len/4, y-len/2, len/2);
 	}
+	else 
+	{
+		strokeWeight(1);
+		triangle(x, y, x+len/2, y-len, x+len, y);
+	}
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalFractal" };
     if (passedArgs != null) {
